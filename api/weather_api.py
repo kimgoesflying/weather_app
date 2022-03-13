01d6916945a736dd16559510f74a610ed438d166
kimgoesflying <yelizarova.ae@gmail.com>
2022-03-13 22:12:56 +0300

    init

---
+++ api/weather_api.py
@@ -0,0 +1,14 @@
+import fastapi
+from fastapi import Depends
+from typing import Optional
+from models.location import Location
+from services import openweather_service
+router = fastapi.APIRouter()
+
+
+@router.get("/api/weather/{city}")
+async def weather(loc: Location = Depends(), units: Optional[str] = "metric"):
+    report = await openweather_service.get_report(
+        loc.city, loc.state, loc.country, units)
+
+    return report
---
+++ main.py
@@ -0,0 +1,39 @@
+import fastapi
+import uvicorn
+from starlette.staticfiles import StaticFiles
+import json
+from pathlib import Path
+from api import weather_api
+from views import home
+from services import openweather_service
+
+api = fastapi.FastAPI()
+
+
+def configure():
+    configure_routing()
+    configure_api_keys()
+
+
+def configure_api_keys():
+    file = Path("settings.json").absolute()
+    if not file.exists():
+        print(f"WARNING {file} file not found")
+        raise Exception("settings.json file not found")
+
+    with open("settings.json") as fin:
+        settings = json.load(fin)
+        openweather_service.api_key = settings.get("api_key")
+
+
+def configure_routing():
+    api.mount("/static", StaticFiles(directory="static"), name="static")
+    api.include_router(home.router)
+    api.include_router(weather_api.router)
+
+
+if __name__ == "__main__":
+    configure()
+    uvicorn.run(api, port=8000, host="127.0.0.1")
+else:
+    configure()
