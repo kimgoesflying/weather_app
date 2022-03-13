0a7f59b57c36c61131b36b09b2fa1aeb24fcb6b4
kimgoesflying <yelizarova.ae@gmail.com>
2022-03-13 23:03:34 +0300

    add validation

--- api/weather_api.py
+++ api/weather_api.py
@@ -2,13 +2,18 @@ import fastapi
 from fastapi import Depends
 from typing import Optional
 from models.location import Location
+from models.validation_error import ValidationError
 from services import openweather_service
 router = fastapi.APIRouter()
 
 
 @router.get("/api/weather/{city}")
 async def weather(loc: Location = Depends(), units: Optional[str] = "metric"):
-    report = await openweather_service.get_report(
-        loc.city, loc.state, loc.country, units)
+    try:
+        return await openweather_service.get_report(
+            loc.city, loc.state, loc.country, units)
+    except ValidationError as ve:
+        return fastapi.Response(content=ve.error_msg, status_code=ve.status_code)
 
-    return report
+    except Exception as x:
+        return fastapi.Response(content=str(x), status_code=500)
--- services/openweather_service.py
+++ services/openweather_service.py
@@ -1,10 +1,15 @@
-from typing import Optional
+from typing import Optional, Tuple
 import httpx
+from httpx import Response
 from infrastracture import weather_cache
+from models.validation_error import ValidationError
+
 api_key: Optional[str] = None
 
 
 async def get_report(city: str, state: Optional[str], country: str, units: str) -> dict:
+    city, state, country, units = validate_units(city, state, country, units)
+
     if forecast := weather_cache.get_weather(city, state, country, units):
         return forecast
 
@@ -16,8 +21,9 @@ async def get_report(city: str, state: Optional[str], country: str, units: str) 
     url = f"https://api.openweathermap.org/data/2.5/weather?q={q}&appid={api_key}&units={units}"
 
     async with httpx.AsyncClient() as client:
-        resp = await client.get(url)
-        resp.raise_for_status()
+        resp: Response = await client.get(url)
+        if resp.status_code != 200:
+            raise ValidationError(resp.text, status_code=resp.status_code)
 
     data = resp.json()
     forecast = data['main']
@@ -25,3 +31,33 @@ async def get_report(city: str, state: Optional[str], country: str, units: str) 
     weather_cache.set_weather(city, state, country, units, forecast)
 
     return forecast
+
+
+def validate_units(city: str, state: Optional[str], country: Optional[str], units: str) -> \
+        Tuple[str, Optional[str], str, str]:
+    city = city.lower().strip()
+    if not country:
+        country = "ru"
+    else:
+        country = country.lower().strip()
+
+    if len(country) != 2:
+        error = f"Invalid country: {country}. It must be a two letter abbreviation such as US or GB."
+        raise ValidationError(status_code=400, error_msg=error)
+
+    if state:
+        state = state.strip().lower()
+
+    if state and len(state) != 2:
+        error = f"Invalid state: {state}. It must be a two letter abbreviation such as CA or KS (use for US only)."
+        raise ValidationError(status_code=400, error_msg=error)
+
+    if units:
+        units = units.strip().lower()
+
+    valid_units = {'standard', 'metric', 'imperial'}
+    if units not in valid_units:
+        error = f"Invalid units '{units}', it must be one of {valid_units}."
+        raise ValidationError(status_code=400, error_msg=error)
+
+    return city, state, country, units
